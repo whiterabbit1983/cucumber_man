58d4f0a2a29fea6d937788074800fa3a3d289ff8
Dmitry Paramonov <asmatic075@gmail.com>
2018-07-31 19:32:12 +0300

    initial commit

---
+++ Assets/Scripts/ReactiveTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactiveTarget : MonoBehaviour {
+	public void ReactToHit () {
+		WanderingAI behaviour = GetComponent<WanderingAI>();
+		if (behaviour != null) {
+			behaviour.Alive = false;
+		}
+		StartCoroutine(Die());
+	}
+
+	private IEnumerator Die () {
+		this.transform.Rotate(-75, 0, 0);
+		yield return new WaitForSeconds(1.5f);
+		Destroy(this.gameObject);
+	}
+}
